65cdd8ed16f742757dc4332219b9d6a100ccafd9
Borruso <gborruso@dinamicheaziendali.it>
2023-07-28 12:23:11 +0200

    [UPD] stock_close_period: aggiornamenti vari

--- stock_close_period/models/stock_move.py
+++ stock_close_period/models/stock_move.py
@@ -19,14 +19,15 @@ class StockMoveLine(models.Model):
 
     # add field to manage closed lines
     active = fields.Boolean(related="move_id.active", store=True, default=True)
+    company_id = fields.Many2one(related="move_id.company_id", store=True)
 
-    def recompute_average_cost_period_purchase(self):
+    def recompute_average_cost_period_purchase(self, closing_id):
         _logger.info("Recompute average cost period. Making in 2 phases:")
         _logger.info("[1/2] Recompute cost product purchase")
         _logger.info("[2/2] Write results")
 
-        self._recompute_cost_stock_move_purchase()
-        self._write_results()
+        self._recompute_cost_stock_move_purchase(closing_id)
+        self._write_results(closing_id)
 
         _logger.info("End recompute average cost product")
 
@@ -39,6 +40,7 @@ class StockMoveLine(models.Model):
             ("product_id", "=", product_id.id),
             ("date", ">", last_close_date),
             ("active", ">=", 0),
+            ("company_id", "=", company_id),
         ], order="date")
 
         first_move_date = False
@@ -54,12 +56,13 @@ class StockMoveLine(models.Model):
                 storic_price = ph.search([
                     ("product_id", "=", product_id.id),
                     ("datetime", ">=", first_move_date),
+                    ("company_id", "=", company_id),
                 ])
                 if storic_price:
                     storic_price.unlink()
 
                 # get start data from last close
-                start_qty, start_price = self._get_last_closing(product_id.id)
+                start_qty, start_price = self._get_last_closing(closing_id, product_id.id, company_id)
 
                 # se valorizzata, crea la prima riga sullo storico prezzi
                 if start_qty:
@@ -98,7 +101,28 @@ class StockMoveLine(models.Model):
 
                 # è un vero PO da mediare
                 # fa prevalere vale il prezzo sul PO nel caso sia stato aggiornato
-                price = move_id.purchase_line_id.price_unit
+                try:
+                    distribution_obj = self.env["purchase.cost.distribution.line"]
+                    distribution_line_id = distribution_obj.sudo().search([
+                        ("move_id", "=", move_id.id),
+                        ("company_id", "=", company_id),
+                    ])
+                    standard_price_new = distribution_line_id.standard_price_new
+                except:
+                    standard_price_new = 0
+
+                if standard_price_new:
+                    price = standard_price_new
+                elif move_id.purchase_line_id.currency_id == move_id.purchase_line_id.company_id.currency_id:
+                    price = move_id.purchase_line_id.price_unit
+                else:
+                    price = move_id.purchase_line_id.currency_id._convert(
+                        move_id.purchase_line_id.price_unit,
+                        move_id.purchase_line_id.company_id.currency_id,
+                        move_id.purchase_line_id.company_id,
+                        move_id.date,
+                        False
+                    )
                 if move_id.price_unit != price:
                     new_price = price
                     move_id.price_unit = new_price
@@ -162,7 +186,15 @@ class StockMoveLine(models.Model):
     def _get_cost_stock_move_standard(self, product_id, closing_id, company_id, closing_line_id):
         # ricalcola std_cost
         # recupera il prezzo standard alla data del movimento
-        price_unit = product_id.get_history_price(company_id, closing_id.close_date)
+        history_price = self.env["product.price.history"].search([
+            ("company_id", "=", company_id),
+            ("product_id", "in", product_id.ids),
+            ("datetime", ">", closing_id.close_date),
+        ])
+        if history_price:
+            price_unit = product_id.get_history_price(company_id, closing_id.close_date)
+        else:
+            price_unit = product_id.standard_price
 
         # se non trova std_cost, prende il prezzo ora disponibile
         if price_unit == 0:
@@ -172,7 +204,7 @@ class StockMoveLine(models.Model):
         closing_line_id.price_unit = price_unit
         closing_line_id.evaluation_method = "standard"
 
-    def _recompute_cost_stock_move_purchase(self):
+    def _recompute_cost_stock_move_purchase(self, closing_id):
         #
         #   Aquisti: Prezzo medio ponderato nel periodo. Esempio:
         #
@@ -189,8 +221,6 @@ class StockMoveLine(models.Model):
         sm = self.env["stock.move"]
         ph = self.env["product.price.history"]
 
-        # get closing_id
-        closing_id = wcp.search([("state", "=", "confirm")], limit=1)
         # search only lines not elaborated
         closing_line_ids = wcpl.search([
             ("close_id", "=", closing_id.id),
@@ -199,7 +229,14 @@ class StockMoveLine(models.Model):
         ])
 
         # get last_close_date
-        last_closed_id = wcp.search([("state", "=", "done")], order="close_date desc", limit=1)
+        if closing_id.last_closed_id:
+            last_closed_id = closing_id.last_closed_id
+        else:
+            last_closed_id = wcp.search([
+                ("state", "=", "done"),
+                ("company_id", "=", company_id)
+            ], order="close_date desc", limit=1)
+
         if last_closed_id:
             # get from last closed
             last_close_date = last_closed_id.close_date
@@ -211,23 +248,26 @@ class StockMoveLine(models.Model):
         for closing_line_id in closing_line_ids:
             product_id = closing_line_id.product_id
 
-            # solo prodotti valutati al medio o standard
-            if product_id.categ_id.property_cost_method == "average":
-                self._get_cost_stock_move_purchase_average(
-                    product_id, last_close_date, sm, ph, company_id, closing_line_id, closing_id)
-            if product_id.categ_id.property_cost_method == "standard":
-                self._get_cost_stock_move_standard(product_id, closing_id, company_id, closing_line_id)
+            if closing_id.force_evaluation_method != "no_force" and not closing_line_id.evaluation_method:
+                if closing_id.force_evaluation_method == "purchase":
+                    self._get_cost_stock_move_purchase_average(
+                        product_id, last_close_date, sm, ph, company_id, closing_line_id, closing_id)
+                if closing_id.force_evaluation_method == "standard":
+                    self._get_cost_stock_move_standard(product_id, closing_id, company_id, closing_line_id)
+            else:
+                # solo prodotti valutati al medio o standard
+                if product_id.categ_id.property_cost_method == "average":
+                    self._get_cost_stock_move_purchase_average(
+                        product_id, last_close_date, sm, ph, company_id, closing_line_id, closing_id)
+                if product_id.categ_id.property_cost_method == "standard":
+                    self._get_cost_stock_move_standard(product_id, closing_id, company_id, closing_line_id)
 
             self.env.cr.commit()
         _logger.info("[1/2] Finish recompute average cost product")
 
-    def _write_results(self):
-        wcp = self.env["stock.close.period"]
+    def _write_results(self, closing_id):
         wcpl = self.env["stock.close.period.line"]
         decimal = dp.get_precision("Product Price")(self._cr)[1]
-
-        # get closing_id
-        closing_id = wcp.search([("state", "=", "confirm")], limit=1)
 
         # search lines
         closing_line_ids = wcpl.search([("close_id", "=", closing_id.id)])
@@ -246,7 +286,7 @@ class StockMoveLine(models.Model):
 
         _logger.info("[2/2] Finish writing results")
 
-    def _get_last_closing(self, product_id):
+    def _get_last_closing(self, closing_id, product_id, company_id):
         wcp = self.env["stock.close.period"]
         wcpl = self.env["stock.close.period.line"]
 
@@ -254,11 +294,17 @@ class StockMoveLine(models.Model):
         start_qty = 0
         start_price = 0
 
-        # get last closing_id
-        closing_id = wcp.search([("state", "=", "done")], order="close_date desc", limit=1)
+        if closing_id.last_closed_id:
+            last_closed_id = closing_id.last_closed_id
+        else:
+            last_closed_id = wcp.search([
+                ("state", "=", "done"),
+                ("company_id", "=", company_id)
+            ], order="close_date desc", limit=1)
+
         # search product
         closing_line_id = wcpl.search([
-            ("close_id", "=", closing_id.id),
+            ("close_id", "=", last_closed_id.id),
             ("product_id", "=", product_id)
         ], limit=1)
 
