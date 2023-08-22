ab65fd9206f3b18b85601d688a30d960419a7653
Borruso <gborruso@dinamicheaziendali.it>
2023-08-22 16:35:37 +0200

    [UPD] stock_close_period: calcolo magazzino

--- stock_close_period/__manifest__.py
+++ stock_close_period/__manifest__.py
@@ -21,7 +21,6 @@
     "data": [
         "security/stock_close_group.xml",
         "security/ir.model.access.csv",
-        "data/action_server.xml",
         "views/stock_close_views.xml",
         "wizards/stock_close_import.xml",
         "wizards/stock_close_print.xml",
