270930c051eb8baf9f1855bf6135a15eea0cd20a
sergiocorato <sergiocorato@gmail.com>
2024-05-31 11:11:26 +0200

    [ADD] module to save splitted method with specific depend

---
+++ stock_close_period_landed_costs/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2023-Today:
+# Dinamiche Aziendali Srl (<http://www.dinamicheaziendali.it/>)
+# @author: Marco Calcagni <mcalcagni@dinamicheaziendali.it>
+# @author: Giuseppe Borruso <gborruso@dinamicheaziendali.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Close Period Landed Costs",
+    "summary": "Glue module with Stock Close Period and Stock Landed Costs",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "author": "Pordenone Linux User Group (PNLUG), Odoo Community Association (OCA),"
+    "DinamicheAziendali",
+    "website": "https://github.com/DinamicheAziendali/stock_close_period",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_close_period",
+        "stock_landed_costs",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
