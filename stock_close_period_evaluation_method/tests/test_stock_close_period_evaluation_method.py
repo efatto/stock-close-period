3cbbd5a19a58d173a88b3fb0612742d33fe82eb9
sergiocorato <sergiocorato@gmail.com>
2024-12-17 18:44:12 +0100

    [IMP] other tests

--- stock_close_period_evaluation_method/tests/test_stock_close_period_evaluation_method.py
+++ stock_close_period_evaluation_method/tests/test_stock_close_period_evaluation_method.py
@@ -1,6 +1,8 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from datetime import timedelta
 
+from odoo import fields
 from odoo.tests.common import Form
 
 from odoo.addons.stock_move_backdating.tests.common import TestCommon
@@ -50,7 +52,8 @@ class TestPicking(TestCommon):
             }
         )
 
-    def _create_purchase_order(self, product_qty, price_unit):
+    def _create_purchase_order_backdate(self, product_qty, price_unit, days_backdating):
+        date_backdating = self._get_datetime_backdating(days_backdating)
         purchase_order_form = Form(self.env["purchase.order"].with_user(self.test_user))
         purchase_order_form.partner_id = self.vendor
         with purchase_order_form.order_line.new() as order_line:
@@ -59,33 +62,25 @@ class TestPicking(TestCommon):
             order_line.price_unit = price_unit
         purchase_order = purchase_order_form.save()
         purchase_order.button_confirm()
-        return purchase_order
-
-    def test_00_purchase_order(self):
-        purchase_order = self._create_purchase_order(product_qty=10, price_unit=5)
         self.assertEqual(
             len(purchase_order.order_line), 1, msg="Order line was not created"
         )
-        date_backdating = self._get_datetime_backdating(365)
         self.assertEqual(len(purchase_order.picking_ids), 1)
         picking = purchase_order.picking_ids
         self.picking = picking
         self._transfer_picking_with_dates(date_backdating)
 
-        purchase_order = self._create_purchase_order(product_qty=10, price_unit=5)
-        self.assertEqual(
-            len(purchase_order.order_line), 1, msg="Order line was not created"
-        )
-        date_backdating = self._get_datetime_backdating(365)
-        self.assertEqual(len(purchase_order.picking_ids), 1)
-        picking = purchase_order.picking_ids
-        self.picking = picking
-        self._transfer_picking_with_dates(date_backdating)
+    def test_00_stock_close(self):
+        self._create_purchase_order_backdate(
+            product_qty=10, price_unit=5, days_backdating=365)
+        self._create_purchase_order_backdate(
+            product_qty=10, price_unit=5, days_backdating=365)
 
         stock_close_period_form = Form(
             self.env["stock.close.period"].with_user(self.test_user))
         stock_close_period_form.force_evaluation_method = "lifo"
         stock_close_period_form.name = "Stock close evaluation"
+        stock_close_period_form.close_date = fields.Date.today() + timedelta(days=-300)
         stock_close_period = stock_close_period_form.save()
         stock_close_period.action_start()
         self.assertTrue(stock_close_period.line_ids)
@@ -95,3 +90,27 @@ class TestPicking(TestCommon):
         self.assertEqual(stock_close_line.product_qty, 20)
         stock_close_period.action_recalculate_purchase()
         self.assertEqual(stock_close_line.price_unit, 5)
+        stock_close_period.action_done()
+        self.assertEqual(stock_close_period.state, "done")
+
+        self._create_purchase_order_backdate(
+            product_qty=10, price_unit=7, days_backdating=100)
+
+        self._create_purchase_order_backdate(
+            product_qty=10, price_unit=10, days_backdating=90)
+
+        stock_close_period_form1 = Form(
+            self.env["stock.close.period"].with_user(self.test_user))
+        stock_close_period_form1.force_evaluation_method = "lifo"
+        stock_close_period_form1.name = "Stock close evaluation"
+        stock_close_period_form1.close_date = fields.Date.today() + timedelta(days=-10)
+        stock_close_period_form1.last_closed_id = stock_close_period
+        stock_close_period1 = stock_close_period_form1.save()
+        stock_close_period1.action_start()
+        self.assertTrue(stock_close_period1.line_ids)
+        stock_close_line1 = stock_close_period1.line_ids.filtered(
+            lambda x: x.product_id == self.product
+        )
+        self.assertEqual(stock_close_line1.product_qty, 40)
+        stock_close_period1.action_recalculate_purchase()
+        self.assertEqual(stock_close_line1.price_unit, 6.75)
