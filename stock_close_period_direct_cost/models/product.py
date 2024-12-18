c108d0d6b282640abb78f8aec06cf081911b500d
sergiocorato <sergiocorato@gmail.com>
2024-12-18 08:36:34 +0100

    [IMP] get cost if direct cost is missing

--- stock_close_period_direct_cost/models/product.py
+++ stock_close_period_direct_cost/models/product.py
@@ -7,4 +7,11 @@ class ProductProduct(models.Model):
     def _get_cost(self):
         # overridable method to customize cost used for evaluation
         super()._get_cost()
-        return self.direct_cost
+        cost = self.direct_cost
+        if not cost:
+            cost = self.standard_price - self.testing_cost
+            # if there is a seller which has 0 as price but a depreciation cost,
+            # subctract it
+            if self.seller_ids:
+                cost -= self.seller_ids[0].depreciation_cost
+        return cost
