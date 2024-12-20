549e8610bcc18582d7f91eb31eda2604af7e3d8c
sergiocorato <sergiocorato@gmail.com>
2024-12-20 10:17:21 +0100

    [FIX] lifo desc

--- stock_close_period_evaluation_method/models/stock_close_period.py
+++ stock_close_period_evaluation_method/models/stock_close_period.py
@@ -7,7 +7,8 @@ class StockClosePeriod(models.Model):
     force_evaluation_method = fields.Selection(
         selection_add=[
             ("fifo", "Compute based FIFO"),
-            ("lifo", "Compute based LIFO"),
+            ("lifo", "Compute based LIFO (continuos)"),
+            # ("lifp", "Compute based LIFO (periodic)"),
         ],
         ondelete={"fifo": "set default", "lifo": "set default"},
         help="Force Evaluation method will be used only for purchase costs computation.",
