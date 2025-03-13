05c7fa05d30e4b54121b3543e0bd9fde01dfc729
sergiocorato <sergiocorato@gmail.com>
2025-03-13 12:36:13 +0100

    [FIX] reuse method to compute unit price on move

--- stock_close_period/models/stock_move.py
+++ stock_close_period/models/stock_move.py
@@ -17,6 +17,47 @@ class StockMove(models.Model):
     # related field to manage closed lines
     active = fields.Boolean(default=True)
 
+    def _get_purchase_price_unit(self):
+        self.ensure_one()
+        if (
+            self.purchase_line_id.invoice_lines
+            and self.purchase_line_id.invoice_lines[0].move_id.state
+            == "posted"
+        ):
+            # In real life, all move lines related to an 1 invoice line
+            # should be in the same state and have the same date
+            inv_line = self.purchase_line_id.invoice_lines[0]
+            # add a check for bad inserted values in invoices (like invoice
+            # a lot of purchased products with 1 in quantity)
+            inv_quantity = inv_line.quantity
+            total_inv_quantity = sum(
+                self.purchase_line_id.invoice_lines.mapped("quantity")
+            )
+            purchase_quantity = self.purchase_line_id.product_uom_qty
+            if inv_quantity < purchase_quantity > total_inv_quantity:
+                inv_quantity = purchase_quantity
+            invoice = inv_line.move_id
+            price_unit = invoice.currency_id._convert(
+                inv_line.price_subtotal,
+                invoice.company_id.currency_id,
+                invoice.company_id,
+                invoice.date or fields.Date.today(),
+            ) / (inv_quantity or 1)
+        else:
+            # get price from purchase line
+            purchase = self.purchase_line_id.order_id
+            price_unit = purchase.currency_id._convert(
+                self.purchase_line_id.price_subtotal,
+                purchase.company_id.currency_id,
+                purchase.company_id,
+                purchase.date_order or fields.Date.today(),
+            ) / (
+                self.purchase_line_id.product_qty
+                if self.purchase_line_id.product_qty != 0
+                else 1
+            )
+        return price_unit or 0.0
+
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
