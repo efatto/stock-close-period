de343a1334ae602f79c8db5cf6beca89ee5d4315
sergiocorato <sergiocorato@gmail.com>
2025-05-21 11:09:36 +0200

    [FIX] recompute amount after validation

--- stock_close_period/models/stock_close.py
+++ stock_close_period/models/stock_close.py
@@ -280,13 +280,13 @@ class StockClosePeriod(models.Model):
     def action_done(self):
         for closing in self:
             closing.state = "done"
-            closing.amount = sum(closing.mapped("line_ids.amount_line"))
             if closing.bypass_negative_qty:
                 closing.line_ids.filtered(lambda x: x.product_qty <= 0).unlink()
             else:
                 closing.line_ids.filtered(
                     lambda x: x.product_qty == 0 and x.price_unit == 0
                 ).unlink()
+            closing.amount = sum(closing.mapped("line_ids.amount_line"))
         return True
 
     def action_recompute_amount(self):
