7da5a558f5666d32b3a8a4e40d59eaca2972145a
sergiocorato <sergiocorato@gmail.com>
2025-06-10 09:35:14 +0200

    [IMP] import price in stock close

---
+++ stock_close_period_import_price/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Close Period Import Prices",
+    "summary": "Add ability to import prices on a stock close period",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/stock-close-period",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_close_period_evaluation_method",
+    ],
+    "data": [
+        "views/stock_close_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ stock_close_period_import_price/models/stock_close_period.py
@@ -0,0 +1,42 @@
+import base64
+
+import xlrd
+
+from odoo import fields, models
+
+
+class StockClosePeriod(models.Model):
+    _inherit = "stock.close.period"
+
+    import_file = fields.Binary(string="Import File with prices")
+    import_file_name = fields.Char(string="Import File Name")
+
+    def action_import_price_file(self):
+        self.ensure_one()
+        decoded_data = base64.decodebytes(self.import_file)
+        wb = xlrd.open_workbook(file_contents=decoded_data)
+        st = wb.sheet_by_index(0)
+        header_row = st.row(0)
+        for rx in range(1, st.nrows):
+            price_unit = False
+            product_default_code = False
+            for cx, c in enumerate(st.row(rx)):
+                if header_row[cx].value == "Costo unitario":
+                    price_unit = c.value
+                if header_row[cx].value == "Prodotto":
+                    product_default_code = c.value
+                if price_unit and product_default_code:
+                    lines = self.line_ids.filtered(
+                        lambda l: l.product_id.default_code == product_default_code
+                    )
+                    if lines:
+                        lines.write(
+                            {
+                                "evaluation_method": "manual",
+                                "price_unit": price_unit,
+                                "evaluation_details": "",
+                            }
+                        )
+                    price_unit = False
+                    product_default_code = False
+        self.action_recompute_amount()
