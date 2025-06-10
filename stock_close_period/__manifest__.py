4108c5e0b55478c89f703c9ce456cd4f086b25d2
sergiocorato <sergiocorato@gmail.com>
2025-06-10 11:20:47 +0200

    [IMP] bump versions

--- stock_close_period/__manifest__.py
+++ stock_close_period/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Stock Close Period",
     "summary": "Stock Close Period",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "category": "Stock",
     "author": "Pordenone Linux User Group (PNLUG), Odoo Community Association (OCA),"
     "DinamicheAziendali, Sergio Corato",
