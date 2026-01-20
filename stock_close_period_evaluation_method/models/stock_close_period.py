cd106681c02add2f30171235967b5a4da9bdb3ee
sergiocorato <sergiocorato@gmail.com>
2026-01-20 10:23:18 +0100

    [IMP] add periodic LIFO

--- stock_close_period_evaluation_method/models/stock_close_period.py
+++ stock_close_period_evaluation_method/models/stock_close_period.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 from odoo.tools import get_lang
 from odoo.tools.float_utils import float_round
 
@@ -10,11 +11,24 @@ class StockClosePeriod(models.Model):
         selection_add=[
             ("fifo", "Compute based FIFO"),
             ("lifo", "Compute based LIFO (continuos)"),
-            # ("lifp", "Compute based LIFO (periodic)"),
+            ("lifp", "Compute based LIFO (periodic)"),
         ],
         ondelete={"fifo": "set default", "lifo": "set default"},
-        help="Force Evaluation method will be used only for purchase costs computation.",
+        help="Force Evaluation method will be used only for purchase costs computation."
+        "FIFO: with FIFO logic;"
+        "LIFO (continuos): with LIFO logic from the beginning of the moves (require "
+        "that a previous closing is not set);"
+        "LIFO (periodic): with LIFO logic from the previous closing (require that a "
+        "previous closing is set).",
     )
+
+    @api.constrains("force_evaluation_method")
+    def _check_force_evaluation_method(self):
+        for closing in self:
+            if closing.force_evaluation_method == "lifo" and closing.last_closed_id:
+                raise UserError(_("You can't set a previous closing."))
+            if closing.force_evaluation_method == "lifp" and not closing.last_closed_id:
+                raise UserError(_("You must set a previous closing."))
 
 
 class StockClosePeriodLine(models.Model):
